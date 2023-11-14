7efb9725118eca3290bb72f895e9ea15799c656a
ariadnesantos <ariadne.semiao1910@gmail.com>
2023-11-14 16:17:18 -0300

    Ajustes na função de validação (--validate implementada)

--- cli.js
+++ cli.js
@@ -5,24 +5,35 @@ const { mdLinks, validateLinks } = require('./index.js');
 const chalk = require('chalk');
 
 const caminhoArquivo = process.argv[2];
+const validateOptionIndex = process.argv.indexOf('--validate'); //Obtém o índice da opção --validate nos argumentos da linha de comando.
 
-mdLinks(caminhoArquivo)
-.then((conteudoArquivo) => {
-    return validateLinks(conteudoArquivo).then(() => {
-        for(const link of conteudoArquivo) {
-            console.log(chalk.bgCyan.white.bold(link.text) +
-            chalk.white.bold(' URL: ') +
-            chalk.cyan(link.url) +
-            chalk.white.bold(' Status: ') +
-            (link.ok === 'ok' ? chalk.green(link.ok) : chalk.red(link.ok))
-            );
+const options = {
+    validate: validateOptionIndex !== -1 //Cria um objeto options que contém a opção validate com base na presença da opção --validate
+};
+
+mdLinks(caminhoArquivo, options)
+.then((conteudoArquivo) => { 
+    if(options.validate) {
+        return validateLinks(conteudoArquivo).then(() => {
+            for(const link of conteudoArquivo) {
+                console.log(
+                    chalk.bgCyan.white.bold(link.text) +
+                    chalk.white.bold(' URL: ') +
+                    chalk.cyan(link.url) +
+                    (link.ok ? chalk.green('☑ OK ') : chalk.red('☒ FAIL ')) +
+                    chalk.white.bold(' Status: ') +
+                    chalk.dim(link.status)
+                );
+            }
+        });
+    } else {
+         // Se não houver opção --validate, apenas imprime os links
+      for (const link of conteudoArquivo) {
+        console.log(chalk.bgCyan.white.bold(link.text) + chalk.white.bold(' URL: ') + chalk.cyan(link.url));
+      }
     }
-    });
 })
+
 .catch((err) => {
     console.error(chalk.red('Erro', err));
-});
-
-
-
-
+  });
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const chalk = require('chalk')
-//const fetch = require('node-fetch')
 
-function mdLinks(caminhoDoArquivo){
+function mdLinks(caminhoDoArquivo, options = {}){ //Parâmetro options é um obj que contém configurações. O padrão é um objeto vazio.
   return new Promise(function(resolve, reject){
     fs.readFile(caminhoDoArquivo, 'utf8', (err, data) => {
       if(err) reject(err);
@@ -15,9 +14,16 @@ let match; //declara uma variável match para armazenar cada correspondência en
 
 //loop while encontra e processa todas as correspondências no texto
 while((match = pattern.exec(data)) !== null) { //.exec  é usado para encontrar todas as correspondências da palavra "regex" no texto.
-    const text = match[1];
-    const url = match[2];
-    links.push({ text, url }); //os objetos de link são adicionados ao array links
+    const text = match[1]; //extração do texto da correspondência
+    const url = match[2]; //extração do link da correspondência
+    const link = { text, url }; //criação de um objeto link com o texto e a URL
+    
+    //Se a opção validate estiver presente, adiciona uma propriedade ok ao objeto link com valor false.
+    if (options.validate) { //valor false indica que a validação ainda não foi realizada 
+      link.ok = false; 
+    }
+
+    links.push(link);
 }
       resolve(links);
     });
@@ -25,20 +31,24 @@ while((match = pattern.exec(data)) !== null) { //.exec  é usado para encontrar 
 }
 //Implementar a função validateLinks, usando fetch
 
-function validateLinks(links) {
-  const linkPromises = links.map((link) => {
-    return fetch(link.url)
+function validateLinks(links) { //função validateLinks aceita uma lista de links
+  const linkPromises = links.map((link) => { //cria um array de Promises para cada link
+    if (link.ok !== undefined) { //verifica se a propriedade 'ok' está presente no link, indicando que a validação é necessária
+      return fetch(link.url) //Usa fetch para fazer uma requisição HTTP ao link e define a propriedade ok com base no status da resposta.
     .then((response) => {
-      if(response.status >= 200 && response.status < 400) {
-        link.ok = chalk.green('☑ OK');
-      } else {
-        link.ok = chalk.red('☒ FAIL');
-      }
+      link.ok = response.status >= 200 && response.status < 400
+      link.status = response.status 
     })
-    .catch(() => (link.ok = chalk.red('☒ FAIL')));
+    .catch(() => {
+      link.ok = false;
+      link.status = 'Link com erro'
+    });
+    } else {
+      return Promise.resolve(); //Se a validação não for necessária, resolve a Promise imediatamente
+    }
   });
-
-  return Promise.all(linkPromises);
+//Retorna uma Promise que será resolvida quando todas as Promises individuais de validação forem concluídas
+  return Promise.all(linkPromises); 
 }
 
 module.exports = { mdLinks, validateLinks };
